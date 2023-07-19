68d29c2cdd6ec8320710e2ff50489aab9b5c81ee
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-07-19 18:55:25 +0530

    Time: 501 ms (64.23%), Space: 173.9 MB (57.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,90 +1,116 @@
-class node :
-    def __init__(self,key,val):
-        self.key=key
-        self.val=val
-        self.prev=None
-        self.next=None 
+struct node{
+    int key;
+    int val; 
+    node* prev;
+    node* next;
+};
+class LRUCache {
+private: 
+    unordered_map<int, node*> map;
+    int cap;
+    node* head;
+    node* tail;
+    
+public:
+    LRUCache(int capacity) {
+        cap=capacity;
+        head = new node();
+        head->key=-1;
+        head->val=-1; 
         
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.head=node(-1,-1) 
-        self.tail=node(-1,-1) 
-        self.head.next=self.tail 
-        self.tail.prev=self.head 
-        self.map={}
-        self.cap=capacity 
-
-    def get(self, key: int) -> int: 
-        if key not in self.map:
-            return -1 
-        existingnode = self.map[key] 
-        ret=existingnode.val
-        # print(ret)
-        existingnode.prev.next  = existingnode.next 
-        existingnode.next.prev = existingnode.prev 
-        existingnode.prev=None 
-        existingnode.next=None 
-        del existingnode
-                
-        headnext=self.head.next 
-        newnode=node(key,ret) 
-        self.head.next=newnode 
-        newnode.prev=self.head 
-        newnode.next = headnext 
-        headnext.prev=newnode
-        self.map[key] = newnode 
-        return ret
-
-    def put(self, key: int, val: int) -> None: 
-        if key in self.map:
-                existingnode = self.map[key] 
-                existingnode.prev.next  = existingnode.next 
-                existingnode.next.prev = existingnode.prev 
-                existingnode.prev=None 
-                existingnode.next=None 
-                del existingnode
-                
-                headnext=self.head.next 
-                newnode=node(key,val) 
-                self.head.next=newnode 
-                newnode.prev=self.head 
-                newnode.next = headnext 
-                headnext.prev=newnode
-                self.map[key] = newnode 
-                
-        elif self.cap>len(self.map): 
-            if key not in self.map :
-                headnext=self.head.next 
-                newnode=node(key,val) 
-                self.head.next=newnode 
-                newnode.prev=self.head 
-                newnode.next = headnext 
-                headnext.prev=newnode
-                self.map[key] = newnode 
-                print(self.map[key], self.head.next.val)
-            
-        else: 
-            deletednode = self.tail.prev 
-            print(self.head.next.val,deletednode.val)
-            deletednode.prev.next = self.tail 
-            self.tail.prev=deletednode.prev 
-            del self.map[deletednode.key]
-            deletednode.prev=None
-            deletednode.next=None 
-            del deletednode
-            
-            headnext=self.head.next 
-            newnode=node(key,val) 
-            self.head.next=newnode 
-            newnode.prev=self.head 
-            newnode.next = headnext 
-            headnext.prev=newnode
-            self.map[key] = newnode
-                
+        tail= new node(); 
+        tail->key=-1;
+        tail->val=-1; 
+    
+        head->next=tail;
+        tail->prev=head; 
+    
+    }
+    
+    int get(int key) {
+        if(map.find(key)==map.end())
+            return -1;
         
+        node* existingnode = map[key] ;
+        int ret=existingnode->val;
+        existingnode->prev->next  = existingnode->next ;
+        existingnode->next->prev = existingnode->prev ;
+        existingnode->prev=NULL;
+        existingnode->next=NULL; 
+        delete existingnode;
+                
+        node* headnext=head->next;
+        node* newnode=new node();
+        newnode->key=key;
+        newnode->val=ret;
+        head->next=newnode;
+        newnode->prev=head;
+        newnode->next = headnext; 
+        headnext->prev=newnode;
+        map[key] = newnode; 
+        return ret;
+    }
+    
+    void put(int key, int val) { 
+        if(map.find(key)!=map.end())
+        {
+            node *existingnode = map[key]; 
+            existingnode->prev->next  = existingnode->next; 
+            existingnode->next->prev = existingnode->prev ;
+            existingnode->prev=NULL;
+            existingnode->next=NULL;
+            delete existingnode;
+                
+            node *headnext=head->next;
+            node *newnode=new node();
+            newnode->key=key;
+            newnode->val=val;
+            head->next=newnode ;
+            newnode->prev=head ;
+            newnode->next = headnext ;
+            headnext->prev=newnode;
+            map[key] = newnode; 
+        } 
+        
+        else if (cap>map.size()){ 
+            
+            node *headnext=head->next; 
+            node *newnode=new node();
+            newnode->key=key;
+            newnode->val=val;
+            head->next=newnode; 
+            newnode->prev=head;
+            newnode->next = headnext; 
+            headnext->prev=newnode;
+            map[key] = newnode;
+        }
+            
+        else{ 
+            node *deletednode = tail->prev;
+            deletednode->prev->next = tail;
+            tail->prev=deletednode->prev;
+            map.erase(deletednode->key); 
+            deletednode->prev=NULL;
+            deletednode->next=NULL;
+            delete deletednode;
+            
+            node* headnext=head->next;
+            node* newnode=new node();
+            newnode->key=key;
+            newnode->val=val;
+            head->next=newnode; 
+            newnode->prev=head ;
+            newnode->next = headnext ;
+            headnext->prev=newnode;
+            map[key] = newnode;
+        }
+        
+    }
+};
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
