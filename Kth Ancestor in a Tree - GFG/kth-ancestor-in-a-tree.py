9b87ca3228c781e6905df78ac741b80c0417389d
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-08-04 20:31:54 +0530

    Added solution - LeetHub

---
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.py
@@ -0,0 +1,112 @@
+#User function Template for python3
+
+def getBFSParentNodeData(root , node):
+    first_data = root
+    queue = [first_data]
+    parent_node_data = {root.data : None}
+    while queue : 
+        root = queue.pop(0)
+        if root.left :
+            parent_node_data[root.left.data] = root.data
+            queue.append(root.left)
+            if root.left.data == node:
+                break
+        if root.right : 
+            parent_node_data[root.right.data] = root.data
+            queue.append(root.right)
+            if root.right.data == node  :
+                break
+    return parent_node_data
+            
+    
+def kthAncestor(root,k, node):
+    parent_node_data = getBFSParentNodeData(root , node)
+    for _ in range(k):
+        new_parent = parent_node_data.get(node)
+        if not new_parent :
+            return -1
+        else :
+            node = new_parent
+    return node
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while size > 0 and i < len(ip):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        k,node=[int(x) for x in input().split()]
+        s = input()
+
+        root = buildTree(s)
+        print(kthAncestor(root,k,node))
+# } Driver Code Ends
