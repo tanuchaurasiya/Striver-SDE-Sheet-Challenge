07f9a2d96c7155e8b5f83832ac56894613e6b980
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-08-05 00:48:30 +0530

    Time: 8 ms (71.77%), Space: 14.2 MB (26.04%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -8,17 +8,19 @@ public:
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         // Code here 
+        if(image[sr][sc]==newColor) 
+            return image;
         int r=image.size();
         int c=image[0].size();
         int clr = image[sr][sc];
         queue<pair<int,int>> q; 
-        q.push({sr,sc}); 
+        q.push({sr,sc});  
         while(!q.empty())
         { 
             int i = q.front().first;
             int j = q.front().second; 
             q.pop();
-            image[i][j]=-1;
+            image[i][j]=newColor;
             if(isValid(i-1,j,r,c,clr,image) )
                 q.push({i-1,j});
                 
@@ -32,14 +34,7 @@ public:
                 q.push({i,j+1});
                 
         }
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                if(image[i][j]<0)
-                    image[i][j]=newColor;
-            }
-        }
+        
         return image;
     }
 };
