de9021fa3230ca0ccb8f196b470cab6e2c8cfb94
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-01 11:31:13 +0530

    Time: 3 ms (83.23%), Space: 13.9 MB (36.99%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,32 +12,27 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) { 
-        vector<vector<TreeNode*>> res; 
         vector<vector<int>> ans;
         if(root==NULL) 
             return ans;
         vector<TreeNode*> temp; 
-        temp.push_back(root); 
-        while(temp.size()>0){  
-            res.push_back(temp);
-            vector<TreeNode*> temp1; 
-            for(auto c: temp){
+        temp.push_back(root);  
+        vector<int> v;
+        while(temp.size()>0){   
+            vector<TreeNode*> temp1;  
+            for(auto c: temp){ 
+                v.push_back(c->val);
                 if(c->left) 
                     temp1.push_back(c->left); 
                 if(c->right) 
                     temp1.push_back(c->right); 
             } 
+            ans.push_back(v); 
+            v.clear();
             temp=temp1; 
         } 
         
-        for(auto list: res){ 
-            vector<int> v;
-            for(auto node: list){
-                v.push_back(node->val);
-            } 
-            
-            ans.push_back(v);
-        } 
+        
         return ans;
     }
 };
