29d5b5bf0b921eff41716478ef8fbbc828e38fa1
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-03 10:35:36 +0530

    Added solution - LeetHub

--- Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
@@ -45,30 +45,79 @@ struct Node
 
 class Solution{
   public:
-    //Function to merge K sorted linked list.
-    Node * mergeKLists(Node *lists[], int K)
-    {
-           // Your code here
-           vector<int> v; 
-        for(int i=0;i<K;i++){
-            Node* t = lists[i];
-            while (t){
-                v.push_back(t->data);
-                t=t->next; 
-            }
+    Node* mergeTwoList(Node* l1, Node* l2){
+        if(l2==NULL) 
+            return l1; 
+        if(l1==NULL) 
+            return l2; 
+        
+        Node* res=NULL; 
+        Node* temp=NULL;
+        if (l1->data < l2->data){ 
+            res=l1; 
+            temp=l1; 
+            l1=l1->next;
+        } 
+        else{
+            res=l2; 
+            temp=l2; 
+            l2=l2->next;
         } 
         
-        sort(v.begin(),v.end()); 
-        
-        Node* temp=new Node(0); 
-        Node* res=temp;
-        for(int i=0;i<v.size();i++) {
-            temp->next = new Node(v[i]) ;
+        while(l1 && l2){
+            if (l1->data < l2->data){  
+            temp->next=l1; 
+            l1=l1->next; 
+            temp=temp->next;
+            } 
+        else{ 
+            temp->next=l2; 
+            l2=l2->next; 
             temp=temp->next; 
+            } 
         } 
+        while(l1){
+            temp->next = l1; 
+            temp=temp->next; 
+            l1 = l1->next;
+        } 
+        while(l2){
+            temp->next=l2; 
+            l2=l2->next; 
+            temp=temp->next;
+        } 
+        return res;
+    } 
+    
+    Node* help(vector<Node*> lists)
+    {
+        if(lists.size()==0)
+            return NULL;
+        if(lists.size()==1){
+            return lists[0];
+        }
+        vector<Node* > mergeTwoListVector; 
         
-        return res->next;
+        for(int i=0;i<lists.size();i=i+2){
+            Node* l1 = lists[i];  
+            Node* l2 = NULL;
+            if(i+1<lists.size()) 
+                l2 = lists[i+1]; 
+           Node* merge = mergeTwoList(l1,l2); 
+           mergeTwoListVector. push_back(merge);  
+        }
+        return help(mergeTwoListVector);
     }
+    Node * mergeKLists(Node *arr[], int K) {   
+        vector<Node*> v;
+        for(int i=0;i<K;i++){
+            v.push_back(arr[i]);
+        }
+        
+        return help(v);
+         
+    }
+    
 };
 
 
