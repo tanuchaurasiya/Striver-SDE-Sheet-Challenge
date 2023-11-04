789d73ca4f6ee32331e12e54eea5518f1e6aa40e
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-04 11:42:41 +0530

    Time: 0 ms (100.00%), Space: 7 MB (7.09%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string reorganizeString(string s) { 
+        unordered_map<char, int> mp; 
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            mp[s[i]]+=1;
+        } 
+        cout<<n<<endl;
+        vector<pair<int,char>> counter; 
+
+        for(auto i:mp){ 
+            char c = i.first; 
+            int count = i.second;
+            if(count > int((n+1)/2))
+                return "";
+            pair<int,char>p{count, c};
+            counter.push_back(p); 
+        }
+        
+        sort(counter.rbegin(),counter.rend()); 
+        
+        for(auto i: counter){
+            cout<<i.first<<" "<<i.second<<endl;
+        } 
+        // return "";
+        vector<char> res(n); 
+        int idx=0;
+        for(auto i:counter){
+            int count = i.first;
+            char c = i.second;
+            
+            while(count){
+                res[idx] = c;
+                idx+=2;
+                count-=1;
+         
+                if(idx>=n)
+                    idx=1;
+            }
+            
+        }
+        string op="";
+        for(auto i:res)
+            op+=i;
+        return op;
+        
+        
+    }
+};
