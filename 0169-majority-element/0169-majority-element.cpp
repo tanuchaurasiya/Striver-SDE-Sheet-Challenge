ce364bbaae07e2c87ede0d74bee543778b9b843c
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-05 23:11:47 +0530

    Time: 11 ms (84.09%), Space: 19.9 MB (77.04%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,22 @@
+// watch striver vdo - here moore algo is used
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) { 
+        int count =0 ;
+        int ele; 
+        
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                ele=nums[i];
+                count++;
+            }
+            
+            else if(nums[i] == ele) 
+                count++; 
+            else
+                count--;
+        } 
+        
+        return ele;
+    }
+};
