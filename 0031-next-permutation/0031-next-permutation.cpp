e3362a6e6260c033b1fab174949860827191fc81
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-06 10:43:52 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (44.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,36 @@
 // So we have to implement our own next_permuation fn instead of using stl fn.
 class Solution {
 public:
-    void nextPermutation(vector<int>& A) {
-        next_permutation(A.begin(), A.end());
+    void Findpermutation(vector<int>& a){
+        int n=a.size(); 
+        int idx=-1;
+        for(int i = n-2;i>=0;i--){
+            if(a[i]<a[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        
+        if(idx==-1){
+            reverse(a.begin(), a.end());
+            return;
+        }
+        
+        for(int i=n-1;i>idx;i--){
+            if(a[i]>a[idx]) 
+            {
+                int temp = a[i];
+                a[i]=a[idx];
+                a[idx] = temp;
+                break;
+            }
+        }
+        reverse(a.begin()+idx+1, a.end());
+        return;
+        
+       
+    }
+    void nextPermutation(vector<int>& a) {
+        Findpermutation(a);
     }
 };
