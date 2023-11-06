55d7f4ffe52cd0e52a3d2b29a452682c0cd986ce
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-06 15:21:15 +0530

    Time: 3 ms (84.45%), Space: 11 MB (66.71%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,33 +1,34 @@
+
+
+
 class Solution {
-public: 
-    void help(int i,vector<int> &candidates,int target,int sum,vector<int> &subset,vector<vector<int>> &ans){
-       //Base conditions
-       if(sum == target){
-           ans.push_back(subset);
-           return;
-       }
-       if(sum > target) return;
-       if(i >= candidates.size()) return;
-       
-       //Including ith element
-       subset.push_back(candidates[i]);
-       sum += candidates[i];
-       help(i+1,candidates,target,sum,subset,ans);
-	   cout<<"1 "<<sum<<endl;
-	   //Backtracking Step
-       subset.pop_back();
-       sum -= candidates[i];
-       cout<<sum<<endl;
-       //Skip ith element
-       while(i+1 < candidates.size() && candidates[i] == candidates[i+1]) i++;
-       help(i+1,candidates,target,sum,subset,ans);
-   }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> subset;
-        int sum = 0;
-        sort(candidates.begin(),candidates.end());
-        help(0,candidates,target,sum,subset,ans);
-        return ans;
+  public:
+    void help(int i, int sum, vector<vector<int>> &res, vector<int> &temp, vector<int>& candidates, int target, int n){
+        if(sum==target)
+        {
+            res.push_back(temp);
+            return;
+        }
+        if(sum>target || i>=n)
+            return;
+        
+        // cout<<i<<" "<<sum<<endl;
+        temp.push_back(candidates[i]);
+    
+        help(i+1, sum+candidates[i], res,temp, candidates, target, n);
+        temp.pop_back(); 
+        
+        while(i+1 < n && candidates[i] == candidates[i+1]) i++;
+        help(i+1, sum, res,temp, candidates, target, n);
     }
+   vector<vector<int>> combinationSum2(vector<int>& candidates, int target){
+        vector<vector<int>> res;
+        vector<int> temp; 
+        int n = candidates.size(); 
+        vector<int> v;
+        sort(candidates.begin(), candidates.end());
+        help(0, 0, res, temp, candidates, target, n);
+        return res;
+    }
+    
 };
