3aa0eda64f2163b2613c0987352c2bc17b19d401
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-10 19:50:00 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (82.05%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,28 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public: 
-     TreeNode* help(vector<int>& preorder, vector<int>& inorder, int &preIdx, int inorStart, int inorEnd, unordered_map<int,int> &mp, int n){
-        if((preIdx>= n)  || (inorStart>inorEnd))  
-            return NULL; 
-        
-        int ele=preorder[preIdx++];
-        TreeNode* root=new TreeNode(ele);
-        int idx=mp[ele];
-        
-        root->left = help(preorder, inorder, preIdx, inorStart, idx-1, mp, n); 
-        root->right = help(preorder,inorder, preIdx,idx+1, inorEnd,mp, n);  
+public:
+    TreeNode* help(vector<int>& preorder, int &i, int upper){
+        if(i>=preorder.size() || preorder[i]> upper) return NULL;
+        TreeNode* root=  new TreeNode(preorder[i++]); 
+        root->left = help(preorder, i, root->val);
+        root->right = help(preorder, i, upper);
         return root;
     }
-    
-    TreeNode* bstFromPreorder(vector<int>& preorder) { 
-        vector<int> inorder;
-        for(int i=0;i<preorder.size();i++)
-            inorder.push_back(preorder[i]);
-        sort(inorder.begin(), inorder.end());
-        unordered_map<int,int> mp;
-        for(int i=0;i<inorder.size();i++)
-            mp[inorder[i]]=i;
-        int preIdx=0;
-        return help(preorder, inorder, preIdx, 0, inorder.size()-1, mp, inorder.size());
-        
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int upper = INT_MAX;
+        int i=0;
+        return help(preorder, i, upper);
     }
 };
