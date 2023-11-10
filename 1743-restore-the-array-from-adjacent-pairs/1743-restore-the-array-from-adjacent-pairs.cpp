d9bb0095b05c1f1f628703f773c364ef477b47c0
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-10 13:29:20 +0530

    Time: 484 ms (36.97%), Space: 140.3 MB (45.15%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& adj) {
-        
         unordered_map<int,int> indegree;
         unordered_map<int, vector<int>> adjMap;
-        unordered_map<int,int> visited;
+        
         for(auto t: adj){
             indegree[t[0]]+=1; 
             indegree[t[1]]+=1; 
@@ -12,32 +11,27 @@ public:
             adjMap[t[0]].push_back(t[1]);
             adjMap[t[1]].push_back(t[0]);
         }
-        
         int start;
         for(auto i: indegree){
-            if(i.second==1)
-            {
+            if(i.second==1){ 
                 start=i.first;
                 break;
             }
         }
-        
         int curr = start, prev = INT_MIN;
         vector<int> nums; 
+        unordered_map<int,int> visited;
         while (nums.size() < adj.size()) {
-            cout<<curr<<endl;
             nums.push_back(curr);
+            visited[curr]=1;
             for (auto& next : adjMap[curr]) {
-                if (next != prev) {
-                    prev = curr;
-                    curr = next;
+                if (!visited[next]) {
+                    curr=next;
                     break;
                 }
             }
         }
         nums.push_back(curr);
         return nums;
-        
-        
     }
 };
