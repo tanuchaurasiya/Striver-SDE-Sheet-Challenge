aa2588ec0913f03a4926e5f7e6833fdc20f04742
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-11 22:45:40 +0530

    Time: 70 ms (94.63%), Space: 47.4 MB (85.05%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,27 +11,34 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &v){
+    void inorder(TreeNode* root, vector<TreeNode*> &v){
         if(root==NULL) return;
         
         inorder(root->left, v);
-        v.push_back(root->val);
+        v.push_back(root);
         inorder(root->right, v);
     }
     
-    TreeNode* buildTree(int l, int r, vector<int> &v){
+    TreeNode* buildTree(int l, int r, vector<TreeNode*> &v){
         if(l>r) return NULL;
     
         int m=(l+r)/2;
         
-        TreeNode* root = new TreeNode(v[m]); 
+        TreeNode* root = v[m]; 
         root->left = buildTree(l, m-1, v);
         root->right = buildTree(m+1, r, v);
         return root;
     }
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int>v;
+        vector<TreeNode*>v;
         inorder(root,v);
         return buildTree(0, v.size()-1,v);
     }
 };
+
+
+
+
+
+
+
