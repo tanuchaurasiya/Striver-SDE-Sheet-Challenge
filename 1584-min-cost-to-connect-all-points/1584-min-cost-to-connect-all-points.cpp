8f8f7cbf8734697b3fd86cfae082c0fc12b5f994
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-19 19:12:12 +0530

    Time: 809 ms (27.70%), Space: 238.6 MB (11.86%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    long long find(long long int node, vector<long long int> &graph){
-        if(graph[node]<0) 
-            return node;
-        long long int temp = find(graph[node], graph);
-        graph[node] = temp;
-        return temp;
-    }
-    
-    bool Union(long long int node1, long long int node2, vector<long long int> &graph){
-        long long int n1 = find(node1, graph);
-        long long int n2 = find(node2,  graph);
-        if(n1==n2)
-            return false;
-        
-        if(abs(graph[n1]) >= abs(graph[n2])) {
-            graph[n1] = graph[n1]+graph[n2];
-            graph[n2] = n1;
-        }
-        else{
-            graph[n2] = graph[n1] + graph[n2];
-            graph[n1] = n2;
-        }
-        
-        return true;
-    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        // return 0;
         long long int leng = points.size();
-        
-        vector<vector<long long int>> v;
+        vector<vector<pair<long long int, long long int>>> adj(leng);
         for(long long int i=0;i<leng;i++){
-            for(long long int j=i+1;j<leng;j++)
-                v.push_back({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i,j});
+            for(long long int j=i+1;j<leng;j++){
+                adj[i].push_back({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),j});
+                adj[j].push_back({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i});
+            }
         }
         
-        sort(v.begin(), v.end());
-        vector<long long int> graph(leng,-1);
-        int res=0;
-        
-        for(auto temp:v){
-            if(Union(temp[1], temp[2], graph))
-                res+=temp[0];
-        }
-        
+        priority_queue<pair<long long int,long long int>, vector<pair<long long int, long long int>>, greater<pair<long long int, long long int>>> pq;
+        long long int start=0;
+        pq.push({0, start});
+        vector<long long int> visited(leng);
+        long long int res=0;
+        while(!pq.empty()){
+            long long int d = pq.top().first;
+            long long int n = pq.top().second;
+            pq.pop();
+            if(visited[n]==1) continue;
+            // cout<<"d and n "<<d<<" "<<n<<endl;
+            res+=d;
+            visited[n] = 1;
+            
+            for(auto child : adj[n]){
+                if(!visited[child.second]){
+                    pq.push({child.first, child.second});
+                    // cout<<"child weight and node "<<child.first<<" "<<child.second<<endl;
+                }
+            }
+        } 
+    
         return res;
+        
     }
 };
