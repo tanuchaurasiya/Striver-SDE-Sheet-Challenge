7f72e22a59ce43c9548e3d184c14b9be5414fa9d
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-20 14:09:17 +0530

    Time: 954 ms (5.05%), Space: 777.8 MB (5.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    bool solve(string &s,string &p,int n,int m,vector<vector<int>> &dp){
-        if(n==0 && m==0) return true;
-        if(m==0 && n>0) return false;
-        if(m>0 && n==0){
-            for(int i=1;i<=m;i++){
-               if(p[i-1]!='*') return false;
+    bool help(int i, int j, string s, string p, int n, int m, vector<vector<int>>&dp){
+        if(i>=n && j>=m) 
+            return true;
+        if(i>=n && j<m){
+            while(j<m ){
+                if(p[j]!='*') return false;
+                j++;
             }
             return true;
         }
-        if(dp[n][m]!=-1) return dp[n][m];
-
-        if(p[m-1]==s[n-1] || p[m-1]=='?') return dp[n][m]= solve(s,p,n-1,m-1,dp); 
-        else if(p[m-1]=='*') return dp[n][m]=solve(s,p,n-1,m,dp) | solve(s,p,n,m-1,dp);
-        return dp[n][m]=0;
+            
+        if(i<n && j>=m)
+            return false;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        if(s[i]==p[j] || p[j]=='?') 
+            dp[i][j] = help(i+1, j+1, s, p, n,m,dp);
+        else if(p[j]=='*')
+            dp[i][j] = help(i,j+1,s,p,n,m,dp) || help(i+1, j,s,p,n,m,dp);
+        
+        else
+            dp[i][j]=false;
+        return dp[i][j];
+            
     }
     bool isMatch(string s, string p) {
-        int n=s.size(),m=p.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(s,p,n,m,dp);
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        return help(0,0,s,p,n,m, dp);
     }
 };
