6d05750b980ffa3ef841adf837ecddbf365b3304
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-20 18:17:47 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (9.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,7 +10,16 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return help(n-1,nums,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=0;
+        
+        for(int i=1;i<=n;i++){
+            int take = nums[i-1];
+            int nottake =0 ;
+            if(i-2>=0) take+=dp[i-2];
+            nottake = dp[i-1]; 
+            dp[i] = max(take,nottake);
+        }
+        return dp[n];
     }
 };
