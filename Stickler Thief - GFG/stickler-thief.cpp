5975d196840e4c3f44c476436615369fb760f79f
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-20 18:23:49 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -7,11 +7,11 @@ typedef long long int ll;
 class Solution
 {
     public:
-    int help(int i, int n, int nums[], vector<int> &dp){
-        if(i>=n) return 0;
+    int help(int i, int nums[], vector<int> &dp){
+        if(i<0) return 0;
         if(dp[i]!=-1) return dp[i];
-        int take = nums[i] + help(i+2, n, nums, dp);
-        int nottake = help(i+1,n,nums, dp);
+        int take = nums[i] + help(i-2, nums, dp);
+        int nottake = help(i-1,nums, dp);
         dp[i] = max(take, nottake);
         return dp[i];
     }
@@ -19,7 +19,7 @@ class Solution
     int FindMaxSum(int nums[], int n)
     {
        vector<int> dp(n,-1);
-        return help(0,n,nums,dp);
+        return help(n-1,nums,dp);
     }
 };
 
