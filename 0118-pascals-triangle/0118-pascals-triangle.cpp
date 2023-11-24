06065436fc5b0f684eaff21c45f342b38a76422c
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-24 08:43:54 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (47.01%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,20 +5,19 @@ public:
             return {{1}};
         if(n==2)
             return {{1},{1,1}};
-        vector<int> l{1,1}; 
-        vector<vector<int>> res{{1}, {1,1}};
-        for(long long i=2;i<n;i++){ 
+        vector<vector<int>> l{{1},{1,1}};
+        for(int i=2;i<n;i++){ 
             vector<int> l1;
             l1.push_back(1); 
             int j=0;
             while(j<i-1){
-                l1.push_back((l[j]+l[j+1]));
+                l1.push_back(l[i-1][j]+l[i-1][j+1]);
                 j+=1;
             }
             l1.push_back(1);
-            l = l1;
-            res.push_back(l);
+            l.push_back(l1);
+       
         }
-        return res;
+        return l;
     }
 };
