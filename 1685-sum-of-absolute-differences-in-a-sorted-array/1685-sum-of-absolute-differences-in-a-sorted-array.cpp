e17006e594e581e75a99fc0452de61b94d5b1a72
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-25 11:51:00 +0530

    Time: 81 ms (92.43%), Space: 88.2 MB (24.86%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -4,18 +4,17 @@ public:
         int n = nums.size();
         vector<int> result(n);
         vector<int> prefixSum(n), suffixSum(n);
-        
-        // Calculate and initialize the prefix sums & suffixSum array
-        prefixSum[0] = nums[0];
-        suffixSum[n - 1] = nums[n - 1];
-        
-        // Calculate the prefix sums & suffixSum array in one loop
-        for (int i = 1; i < n; ++i) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-            suffixSum[n - i - 1] = suffixSum[n - i] + nums[n - i - 1];
+        int prefix=0;
+        for (int i = 0; i < n; ++i) {
+            prefixSum[i] = prefix;
+            prefix+=nums[i];
+        }
+        int suffix=0;
+        for (int i = n-1; i >=0; i--) {
+            suffixSum[i] = suffix;
+            suffix+=nums[i];
         }
         
-        // Calculate absolute differences and update the result array
         for (int i = 0; i < n; ++i) {
             int currentAbsoluteDiff = ((nums[i] * i) - prefixSum[i]) + (suffixSum[i] - (nums[i] * (n - i - 1)));
             result[i] = currentAbsoluteDiff;
