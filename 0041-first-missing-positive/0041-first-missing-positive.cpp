cf86878a0a9cbff7f6694582d20f986fc942259d
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-11-26 20:10:50 +0530

    Time: 45 ms (83.26%), Space: 41.6 MB (50.92%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public: 
-//     TC - nlogn 
+//     TC - o(n) SC- o(1)
+//     the missing number will always belong to {1, length(arr)+1}
     int firstMissingPositive(vector<int>& arr) {
-        unordered_map<int,int> mp;
         int n = arr.size();
-        for(int i=0;i<n;i++)
-            mp[arr[i]] = 1;
-        
-        for(int i=1;i<=n+1;i++)
-            if(mp[i]==0) return i; 
+        for(int i=0;i<n;i++){
+            if(arr[i]<0) 
+                arr[i] = 0;
+        }
+         for(int i=0;i<n;i++){
+            if(abs(arr[i])>=1 && abs(arr[i])<=n){
+                int idx = abs(arr[i]) - 1;
+                if(arr[idx]>0) 
+                    arr[idx] = -arr[idx]; 
+                else if(arr[idx]==0)
+                    arr[idx] = -(n+1);
+            }
+
+        }
+        for(int i=1;i<=n;i++)
+            if(arr[i-1]>=0) return i; 
             
-        return -1;
+        return n+1;
     }
 };
