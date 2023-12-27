acad84215a67759b23136f3bed9ae7b23240ece6
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-12-27 13:19:33 +0530

    Time: 124 ms (39.34%), Space: 95.6 MB (99.55%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int res=0; 
-        int i=1;
-        int n = colors.size(); 
-        int prev=0;
-        while(i<n){
-            if(colors[i]==colors[prev]){
-                if(neededTime[i] > neededTime[prev]){
-                    res+=neededTime[prev];
-                    prev=i;
-                    
-                }
-                
-                else{
-                    res+=neededTime[i];
-                    
+        int time=0;
+        int index=0;
+        int sum=neededTime[0];
+        int leng=colors.length();
+        bool flag=false;
+        for(int i=0;i<leng-1;i++){
+            if(colors[i]==colors[i+1]){
+                sum=sum+neededTime[i+1];
+                int t=max(neededTime[i],neededTime[i+1]);
+                index=max(index,t);
+                flag=true;
+                if(i==leng-2){
+                    time=time+sum-index;
                 }
             }
             else{
-                prev=i;
+                if(flag==true){
+                    time=time+sum-index;
+                index=0;
+                sum=neededTime[i+1];
+                    flag=false;
+                }
+                else{
+                    index=0;
+                sum=neededTime[i+1];
+                }
+                
             }
-            i++;
-        } 
-        return res;
-        
+        }
+        return time;
     }
 };
