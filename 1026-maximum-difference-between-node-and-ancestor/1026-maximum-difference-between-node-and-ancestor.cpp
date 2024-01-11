26214ebe8ade7c57c0ac81ea85c5704354a722a7
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2024-01-11 15:34:02 +0530

    Time: 4 ms (53.52%), Space: 12.1 MB (10.26%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,int> help(TreeNode* root, int &ans){
+        if(root==NULL) return {1e6, 0}; //max, min
+        
+        pair<int,int> left = help(root->left, ans);
+        pair<int,int> right = help(root->right, ans); 
+        cout<<"left"<<left.first<<" "<<left.second<<endl;
+        cout<<"right"<<right.first<<" "<<right.second<<endl;
+        if(root->left || root->right)
+            ans =  max(ans, max(abs(root->val-min(left.first, right.first)), abs(root->val - max(left.second, right.second))));
+        cout<<"ans="<<ans<<endl;
+        cout<<"return "<<min(root->val, left.first)<<" "<< max(root->val, right.second)<<endl;
+        return {min(root->val, min(left.first, right.first)), max(root->val, max(left.second,right.second))};
+        
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        int ans=0;
+        pair<int,int> p = help(root, ans);
+        return ans;
+    }
+};
